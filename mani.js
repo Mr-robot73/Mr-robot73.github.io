52bdbb380751e21ba9e0ad3d477e53d09364ff3a
Mr-robot <oscarudk@hotmail.com>
2022-11-12 17:29:51 -0600

    test

--- mani.js
+++ mani.js
@@ -20,7 +20,7 @@ document.getElementById("add").addEventListener("click", () => {
     titule,
     description,
   };
-  array = [...array,objectData];
+  array.push(objectData)
 
   localStorage.setItem("notes", JSON.stringify(array));
   cargar();
